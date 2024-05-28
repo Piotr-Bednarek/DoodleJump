3dcc719584d9978c5cc87efc7ad99f8a9e6467cf
Piotr-Bednarek <72524343+Piotr-Bednarek@users.noreply.github.com>
2024-05-28 22:17:13 +0200

    Added title screen

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 #include "Platform.h"
 #include "Enemy.h"
 
+enum class GameState
+{
+    TITLE,
+    GAME,
+    MULTIPLAYER,
+    GAMEOVER
+
+};
+
 int main()
 {
     srand(time(NULL));
@@ -23,6 +32,8 @@ int main()
     sf::Texture enemy_flying_texture;
     sf::Texture background_texture;
 
+    sf::Font font;
+
     if (!enemy_flying_texture.loadFromFile("assets/enemy/FLYING.png"))
     {
         return 1;
@@ -32,6 +43,24 @@ int main()
     {
         return 1;
     }
+
+    if (!font.loadFromFile("assets/fonts/Jacquard12.ttf"))
+    {
+        return 1;
+    }
+
+    sf::Text title("Doodle Jump!", font, 100);
+    sf::Text info("Press SPACE to start", font, 50);
+
+    sf::FloatRect textRect = title.getLocalBounds();
+    title.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    title.setPosition(sf::Vector2f(WIDTH / 2.0f, 75));
+
+    textRect = info.getLocalBounds();
+    info.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    info.setPosition(sf::Vector2f(WIDTH / 2.0f, 150));
+
+    GameState state = GameState::TITLE;
 
     Enemy enemy1(sf::Vector2f(100, 100), 200, 1.0);
 
@@ -67,20 +96,36 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+            {
+                state = GameState::GAME;
+            }
         }
 
         window.clear();
+
         window.draw(sf::Sprite(background_texture));
 
-        for (const Platform &i : platforms)
+        if (state == GameState::TITLE)
         {
-            window.draw(i);
+            window.draw(title);
+            window.draw(info);
         }
+        else if (state == GameState::GAME)
+        {
+            for (const Platform &i : platforms)
+            {
+                window.draw(i);
+            }
 
-        window.draw(enemy1);
-        enemy1.step();
-        enemy1.move(1.0 / FPS);
+            window.draw(enemy1);
+            enemy1.step();
+            enemy1.move(1.0 / FPS);
+        }
 
         window.display();
     }
